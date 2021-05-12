89dbe88f5955d54b9264aca2aa4da44c9a367311
Sherry1481 <shareyscorpi1@gmail.com>
2021-05-12 17:42:18 +0500

    route groups

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -15,4 +15,17 @@ func routes(_ app: Application) throws {
             req in return "/the great khali"
         }
     
+    app.get("foo","*","baz") { req in
+        return "Foo BazZ"
+    }
+    
+    app.get("search") { req -> String in
+        guard let keyword = req.query["keyword"] as String?,
+              let page = req.query["page"] as Int? else {
+            throw Abort(.badRequest)
+            
+        }
+        return "keyword = \(keyword) and page => \(page)"
+    }
+    
 }
